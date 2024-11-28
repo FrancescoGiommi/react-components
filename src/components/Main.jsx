85389d4b843578c723eed8c235040105abce02ec
FrancescoGiommi <francescogiommi97@icloud.com>
2024-11-28 16:21:43 +0100

    Add details card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,18 @@ export default function Main() {
   return (
     <>
       <main>
-        <div className="container text-center">
-          <div className="card">
-            <div className="img-fluid" style={{ width: "100%" }}>
-              <img src="public/img.png" alt="image" />
+        <div className="container">
+          <div className="card text-start" style={{ width: "500px" }}>
+            <div>
+              <img
+                className="img-fluid rounded"
+                style={{ width: "500px" }}
+                src="public/img.png"
+                alt="image"
+              />
             </div>
             <div className="card-body">
-              <h2 className="card-title">Titolo del post</h2>
+              <h2 className="fs-4 fw-bold">Titolo del post</h2>
               <p className="card-text">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Architecto, dolor animi. Corporis illo asperiores mollitia, qui
@@ -17,7 +22,12 @@ export default function Main() {
                 cum cupiditate fugit possimus expedita.
               </p>
               <div>
-                <button>Leggi di più</button>
+                <button
+                  className="m-2 p-2 btn btn-warning"
+                  style={{ color: "white" }}
+                >
+                  Leggi di più
+                </button>
               </div>
             </div>
           </div>
